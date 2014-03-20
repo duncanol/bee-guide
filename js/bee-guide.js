1d9833c3fec160c47223a29bc0cc60e2ae7f523b
Duncan Watson <duncan.watson@technophobia.com>
2014-03-20 18:22:46 +0000

    Added rotation

--- js/bee-guide.js
+++ js/bee-guide.js
@@ -1,6 +1,25 @@
+function Bee(width, height, x, y) {
+	this.width = width;
+	this.height = height;
+	this.x = x;
+	this.y = y;
+	this.angle = 180;
+}
+
+Bee.prototype.setAttribute = function(attr, value) {
+  this[attr] = value;
+};
+
+Bee.prototype.getAttribute = function(attr) {
+  return this[attr];
+};
+
 jQuery(document).ready(function() {
 
-	var width = 1000, height = 1000, radius = 20;
+	var $window = jQuery(window);
+
+  var width = $window.width() - 20, height = $window.height() - 20, radius = 20;
+	var bee = new Bee(50, 50, width / 2, 50);
 
 	var svg = d3.select("body").append("svg")
     .attr("width", width)
@@ -12,5 +31,29 @@ jQuery(document).ready(function() {
       .size([width, height])
       .radius(radius)
       .mesh);
+
+  var beeImage = svg.append("g")
+  	.append("svg:image")
+  	.attr("xlink:href", "/images/svg/Abeille-bee.svg")
+  	.attr("x", bee.x)
+  	.attr("y", bee.y)
+  	.attr("width", bee.width)
+  	.attr("height", bee.height);
+
+ 	beeImage
+ 		.attr("transform", "rotate(" + bee.angle + ", " + bee.x + ", " + bee.y + ")");
+
+  d3.select(bee)
+    .transition()
+      .duration(1000)
+      .attrTween("x", tween)
+      .attr("x", 100);
+
+
+  beeImage
+    .attr("x", bee.x)
+    .attr("y", bee.y)
+    .attr("transform", "rotate(" + bee.angle + ", " + bee.x + ", " + bee.y + ")");
+
 
 });
